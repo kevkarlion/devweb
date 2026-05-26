f6e7acebe365264759eda40c05fb9b18b079f803
Kevin Riquelme <kriquelme10@gmail.com>
2026-05-26 15:21:09 +0000

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -122,25 +122,25 @@ export default function RootLayout({
   return (
     <html lang="es" className="dark w-screen">
       <head>
-        <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link
           rel="icon"
           type="image/png"
-          href="/favicon/favicon-32x32.png"
+          href="/favicon-32x32.png"
           sizes="32x32"
         />
         <link
           rel="icon"
           type="image/png"
-          href="/favicon/favicon-16x16.png"
+          href="/favicon-16x16.png"
           sizes="16x16"
         />
         <link
           rel="apple-touch-icon"
-          href="/favicon/apple-touch-icon.png"
+          href="/apple-touch-icon.png"
           sizes="180x180"
         />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="manifest" href="/site.webmanifest" />
 
         {/* ✅ Preconexiones para mejor performance */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
